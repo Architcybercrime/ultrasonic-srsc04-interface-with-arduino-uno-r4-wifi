13e40462d5ce4245b92cae6ab706bbf5960d1f2f
Architcybercrime <nishaagrawal747@gmail.com>
2026-02-03 09:47:36 +0530

    Improve distance measurement output format in loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
 #define trigPin 9
 #define echoPin 10
+
 void setup() {
-    // write your initialization code here
+    Serial.begin(9600);   // START SERIAL MONITOR
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 }
 
 void loop() {
-    // write your code here
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
+
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
+
     long duration = pulseIn(echoPin, HIGH);
     long distance = duration * 0.034 / 2;
-    Serial.println("distance:");
-    Serial.println(distance);
-    Serial.println("cm");
+
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
     delay(1000);
+}
