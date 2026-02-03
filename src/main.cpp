3fd98dcd1156b0317c0676a434b83d567c272456
Architcybercrime <nishaagrawal747@gmail.com>
2026-02-03 09:23:00 +0530

    Add pin Configuration in setup

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#define trigPin 9
+#define echoPin 10
+void setup() {
+    // write your initialization code here
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+void loop() {
+    // write your code here
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.034 / 2;
+    Serial.println("distance:");
+    Serial.println(distance);
+    Serial.println("cm");
+    delay(1000);
